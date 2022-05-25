3609a67742c9359815f1ec184d95afb3cbdb8f5d
Darudul <darudulislam@gmail.com>
2022-05-25 14:18:34 +0800

    added

--- src/Pages/DashBoard/Payment.js
+++ src/Pages/DashBoard/Payment.js
@@ -1,7 +1,13 @@
+import { Elements } from "@stripe/react-stripe-js";
+import { loadStripe } from "@stripe/stripe-js";
 import React from "react";
 import { useQuery } from "react-query";
 import { useParams } from "react-router";
 import Loading from "../../LoginSignup/Loading/Loading";
+import CheckoutForm from "./CheckoutForm";
+const stripePromise = loadStripe(
+  "pk_test_51L3CyrDWtkJH8iSZofaV3dJnvEz6q8gLDUpERbTpyahobDSRRX5c7yi3jO45BS8rJNqWJnTu7SE1uzCxjGdGvPap00gwT42Hsc"
+);
 
 const Payment = () => {
   const { id } = useParams();
@@ -30,9 +36,9 @@ const Payment = () => {
       </div>
       <div class="card flex-shrink-0 w-50 max-w-md shadow-2xl bg-base-100">
         <div class="card-body">
-          {/* <Elements stripe={stripePromise}>
-            <CheckoutForm appointment={appointment} />
-          </Elements> */}
+          <Elements stripe={stripePromise}>
+            <CheckoutForm order={order} />
+          </Elements>
         </div>
       </div>
     </div>
